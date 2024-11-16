f950e591b540ad7681f56069d4e828b42f1001ec
vuongprogk <vuong753333@gmail.com>
2024-11-16 13:41:04 +0700

    feat: add source file and prototype to finish the exercise

---
+++ source.cpp
@@ -0,0 +1,22 @@
+// Version: 1.0
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
+int sum(int a, int b) {
+  // write your code here to caculate the sum of a and b
+}
+
+int sub(int a, int b) {
+  // wrei your code here to caculate the subtract of a and b
+}
+
+int div(int a, int b) {
+  // write your code here to caculate the divide of a and b
+}
+
+int mul(int a, int b) {
+  // write your code here to caculate the multiply of a and b
+}
